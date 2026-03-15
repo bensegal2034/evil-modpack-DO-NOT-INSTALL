ade65ce757445b84bfdd71a2835ed60b41f04e39
Ben Segal <bensegal2034@gmail.com>
2026-03-15 18:33:53 -0400

    packwiz refresh

--- serverpack/run.bat
+++ serverpack/run.bat
@@ -3,6 +3,6 @@ REM Forge requires a configured set of both JVM and program arguments.
 REM Add custom JVM arguments to the user_jvm_args.txt
 REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
 REM  pass them to this script directly
-java -jar packwiz-installer-bootstrap.jar -g -s server http://localhost:8080/pack.toml
+java -jar packwiz-installer-bootstrap.jar -g -s server https://bensegal2034.github.io/evil-modpack-DO-NOT-INSTALL/pack/pack.toml
 java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.10/win_args.txt %*
 pause
